5aac85a663a69c9d03f6231cf1bc9c452b1d8114
Danil.Rutkovskiy <rutkovskiy.danil@mail.ru>
2025-06-13 12:38:55 +0500

    feat: add basic http client sample

--- src/SampleHttpClient/SampleHttpClient.cpp
+++ src/SampleHttpClient/SampleHttpClient.cpp
@@ -1,20 +1,39 @@
-﻿// SampleHttpClient.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+#include "Tester.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	boost::asio::io_context ioContext;
+	{
+		auto workGuard = boost::asio::make_work_guard(ioContext);
+
+		Tester tester(ioContext);
+		tester();
+
+		std::jthread th([&ioContext]() 
+		{
+			try 
+			{
+				ioContext.run();
+				std::cout << "ioContext is out of work" << std::endl;
+			}
+			catch (const std::exception& ex)
+			{
+				std::cout << "exception: " << ex.what() << std::endl;
+			}
+			catch (...)
+			{
+				std::cout << "unknown error" << std::endl;
+			}
+		});
+		std::cout << "Press enter to stop the app" << std::endl;
+		std::cin.get();
+
+		workGuard.reset();
+		ioContext.stop();
+	}
+
+	std::cout << "done" << std::endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
