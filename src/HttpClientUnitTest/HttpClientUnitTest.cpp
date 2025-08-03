3b5b8d45bce8400e74548d47d8a57f9121c8e646
Danil.Rutkovskiy <rutkovskiy.danil@mail.ru>
2025-08-03 11:09:33 +0500

    feat: add unit test

---
+++ src/HttpClientUnitTest/HttpClientUnitTest.cpp
@@ -0,0 +1,26 @@
+﻿#define CATCH_MAIN_CONFIG
+
+#include "../../../../libs/thirdparty/include/catch2/catch_amalgamated.hpp"
+#include "../HttpClient/ConnectionParameters.h"
+#include "../HttpClient/RequestGenerator.h"
+
+TEST_CASE("Http Connection Parameters Parsing")
+{
+	auto p1 = makeConnectionParameters("http://whatever.com/getrequest/path");
+	CHECK(p1.m_host == "whatever.com");
+	CHECK(p1.m_port == "80");
+
+	auto p2 = makeConnectionParameters("http://whatever.com:9812/getrequest/path");
+	CHECK(p2.m_port == "9812");
+
+	auto p3 = makeConnectionParameters("https://whatever.com/getrequest/path");
+	CHECK(p3.m_port == "443");
+}
+
+TEST_CASE("Get Request Generate Parsing")
+{
+	const auto r1 = makeGetRequest("http://httpbin.org/get");
+	CHECK(r1.m_host == "httpbin.org");
+	CHECK(r1.m_verb == HttpVerb::GET);
+	CHECK(r1.m_path == "/get");
+}
