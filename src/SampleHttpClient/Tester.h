5ee262d74d2b96ffcc0f5ebb1ec4ecb1432df631
Danil.Rutkovskiy <rutkovskiy.danil@mail.ru>
2025-08-05 10:19:07 +0500

    feat:: add ssl certificates

--- src/SampleHttpClient/Tester.h
+++ src/SampleHttpClient/Tester.h
@@ -62,12 +62,22 @@ public:
 	{
 		using namespace boost::asio::ssl;
 		auto sslContext = std::make_shared<context>(context::tlsv12);
-		sslContext->set_verify_mode(verify_none);
+
+		sslContext->load_verify_file("cacert.pem");
+		sslContext->set_verify_mode(verify_peer);
 
 		sslContext->set_verify_callback([](bool preverified, verify_context& verifyContext) 
 			{
-				return true;
+				return preverified;
 			});
+
+		const char* defaultCipherList = "HIGH:!ADH:!MD5:!RC4:!SRP:!PSK:!DSS";
+		const auto ret = SSL_CTX_set_cipher_list(sslContext->native_handle(), defaultCipherList);
+		if (!ret)
+		{
+			throw ("can't set cipher list");
+		}
+
 		return sslContext;
 	}
 
